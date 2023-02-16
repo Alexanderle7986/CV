6876d165b29b77ebd48b93f19737a58e0d7eeff4
Minh Canh LE <84127013+Alexanderle7986@users.noreply.github.com>
2023-02-16 12:07:31 +0100

    Update animation.js
    Signed-off-by: Minh Canh LE <84127013+Alexanderle7986@users.noreply.github.com>

--- animation.js
+++ animation.js
@@ -1,22 +1,22 @@
 // phàn contact
 
 
-var index = 1;
-function RunImage() {
-  var imgs = [
-    "contact.jpg",
-    "compétance.jpg",
-    "travail.jpg",
-    "examen.png",
-    "hobbies.jpg"
-  ];
-  document.getElementById("img").src = imgs[index];
-  index++;
-  if (index == 5) {
-    index = 0;
-  }
-}
-setInterval(RunImage, 6000);
+// var index = 1;
+// function RunImage() {
+//   var imgs = [
+//     "contact.jpg",
+//     "compétance.jpg",
+//     "travail.jpg",
+//     "examen.png",
+//     "hobbies.jpg"
+//   ];
+//   document.getElementById("img").src = imgs[index];
+//   index++;
+//   if (index == 5) {
+//     index = 0;
+//   }
+// }
+// setInterval(RunImage, 6000);
 
 
 
